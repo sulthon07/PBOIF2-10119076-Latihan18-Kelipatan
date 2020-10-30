e0509f1bfc4bc8208bb7ea1b8f21ef74d3e7dbfb
ACER <ACER@DESKTOP-JMQ2UKH>
2020-10-30 13:30:45 +0700

    PBOIF2-10119076-Latihan18-Kelipatan

---
+++ src/pboif2/pkg10119076/latihan18/kelipatan/PBOIF210119076Latihan18Kelipatan.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pboif2.pkg10119076.latihan18.kelipatan;
+
+/**
+ *
+ * @author ACER
+ * NAMA     : Sulthon Naufal Akmal
+ * KELAS    : IF2
+ * NIM      : 10119082
+ */
+public class PBOIF210119076Latihan18Kelipatan {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        for (double i = 3.5; i <= 35; i+=3.5){
+               System.out.println(i);
+               
+        }
+        System.out.println("_________________________________");
+        System.out.println("Developed by Sulthon Naufal Akmal");
+    }
+    
+}
